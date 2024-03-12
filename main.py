dfadf54cfb946b65012f18b15c13921d15e14873
Brian Machado <bmachado@live.ca>
2024-03-12 00:56:09 -0400

    fixed af ew things

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from openai import AzureOpenAI
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(override=True)
 
 app = FastAPI()
 
@@ -14,9 +14,9 @@ deployment_name=os.environ.get("AZURE_DEPLOYMENT_NAME")
 
 client = AzureOpenAI(
     api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
-    api_version="2023-12-01-preview",
+    api_version="2024-03-01-preview",
     azure_endpoint = "https://{}.openai.azure.com".format(resource),
-    )
+)
 
 class Message(BaseModel):
     role: str
@@ -28,11 +28,11 @@ class Query(BaseModel):
 
 @app.post("/itsfast")
 async def answer_question(query: Query):
+
     completion_stream = client.chat.completions.create(
         messages=[message.model_dump() for message in query.messages],
         model=deployment_name,
         stream=False,
-        user='fastaskapi'
     )
 
     return {"response": completion_stream.choices[0].message.content}
